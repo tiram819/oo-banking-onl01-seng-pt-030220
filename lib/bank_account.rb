b2aad1ba8a95a97756c3064a6088e2481f453d72
Tabassum Hassan <tiram819@gmail.com>
2020-03-31 23:58:32 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -17,7 +17,9 @@ end
     "Your balance is $#{@balance}."
   end
   
-   
+   def valid?
+    @status == "open" && @balance > 0 ? true : false
+  end
   
   def close_account
     @status = "closed"
