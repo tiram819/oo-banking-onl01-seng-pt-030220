c073ad6a25c4766a8e78584faab8f2a377205ab0
Tabassum Hassan <tiram819@gmail.com>
2020-04-01 00:04:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ class Transfer
   end
     
     def reverse_transfer
-    if @status == "complete"
+    if @status = "complete"
       @sender.balance += @amount
       @receiver.balance -= @amount
       @status = "reversed"
