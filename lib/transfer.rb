9fa35527ca7feefad99202d56702c1ae7ec24813
Tabassum Hassan <tiram819@gmail.com>
2020-04-01 00:05:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,15 +21,15 @@ class Transfer
     if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
-      @status = "complete"
+      @status == "complete"
     else
-      @status = "rejected"
+      @status == "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
     
     def reverse_transfer
-    if @status = "complete"
+    if @status == "complete"
       @sender.balance += @amount
       @receiver.balance -= @amount
       @status = "reversed"
