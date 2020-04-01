bd93ea9a046f417dc5340407d45fd7215ffe6e7e
Tabassum Hassan <tiram819@gmail.com>
2020-04-01 00:27:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,6 +19,7 @@ class Transfer
   
  def execute_transaction
     if @sender.balance > @amount && @status == "pending"
+      &&valid?
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
